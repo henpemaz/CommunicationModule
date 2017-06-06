8c40316c2568dd84e9cdd04a2b3398615cd371a2
Henrique Maziero <henpemaz@gmail.com>
2017-06-06 14:28:27 +0200

    Included shared files into both projects

--- GSMCommunicationModule/GSMCommunicationModule.ino
+++ GSMCommunicationModule/GSMCommunicationModule.ino
@@ -7,18 +7,16 @@
 
 
 
-#include <ArduinoLowPower.h>
 #include "task_scheduler.h"
-
-
-#ifdef __AVR_ATmega32U4__ /* Using ATmega32u4 - GSM module */
-#include <avr/power.h>
 #include "gsm_communication.h"
 
-#elif __SAMD21G18A__ /* Using SAM-D21 - LORA module*/
-#include "lora_communication.h"
-#endif
 
+#include <avr/power.h>
+#include <avr/wdt.h>
+
+
+
+///////////// DEBUG
 void dummy_func() {
 	pinMode(LED_BUILTIN, OUTPUT);
 	digitalWrite(LED_BUILTIN, HIGH);
@@ -36,13 +34,12 @@ void dummy_func() {
 #define sampling_task dummy_func
 #define reporting_task dummy_func
 #define SAMPLING_LOOP_TIME 10
-#define REPORTING_LOOP_TIME 60
-
+#define REPORTING_LOOP_TIME 30
+///////////// \DEBUG
 
 void setup()
 {
 	// Board setup
-#ifdef __AVR_ATmega32U4__ /* Using ATmega32u4 - GSM module */
 
 	wdt_disable();
 	// Shut down unused peripherals
@@ -53,17 +50,7 @@ void setup()
 	power_timer2_disable();
 	power_timer3_disable();
 	power_timer4_disable();  // Requires you to patch iom32u4.h -> add (#define PRTIM4 4) and (#define __AVR_HAVE_PRR1_PRTIM4) and add {| (1 << PRTIM4)} to the existing #define __AVR_HAVE_PRR1
-	#ifndef (_DEBUG | DEBUG | __DEBUG__)
 	power_usb_disable();
-	#endif
-
-#elif __SAMD21G18A__ /* Using SAM-D21 - LORA module*/
-
-	// TODO POWER SAVING FEATURES
-
-#endif
-
-	
 
 	// Set up communication storage and box interface
 
--- LORACommunicationModule/LORACommunicationModule.ino
+++ LORACommunicationModule/LORACommunicationModule.ino
@@ -1,13 +1,66 @@
+
+
+
+
+
+
+
+
+
+#include "task_scheduler.h"
+#include "lora_communication.h"
+
+
+void dummy_func() {
+	pinMode(LED_BUILTIN, OUTPUT);
+	digitalWrite(LED_BUILTIN, HIGH);
+	delay(1000);
+	digitalWrite(LED_BUILTIN, LOW);
+	delay(1000);
+	digitalWrite(LED_BUILTIN, HIGH);
+	delay(1000);
+	digitalWrite(LED_BUILTIN, LOW);
+	delay(1000);
+}
+#define stor_setup dummy_func
+#define sampling_setup dummy_func
+#define reporting_setup dummy_func
+#define sampling_task dummy_func
+#define reporting_task dummy_func
+#define SAMPLING_LOOP_TIME 10
+#define REPORTING_LOOP_TIME 60
+
+
 void setup()
 {
+	// Board setup
 
-  /* add setup code here */
 
+	// TODO POWER SAVING FEATURES
+
+
+
+	// Set up communication storage and box interface
+
+	comm_setup();
+
+	stor_setup();
+
+	sampling_setup();
+
+	reporting_setup();
+
+	// Launch tasks
+	sched_setup();
+
+	sched_add_task(sampling_task, SAMPLING_LOOP_TIME, SAMPLING_LOOP_TIME);
+
+	sched_add_task(reporting_task, REPORTING_LOOP_TIME, REPORTING_LOOP_TIME);
+
+	sched_mainloop();
 }
 
 void loop()
 {
-
-  /* add main program code here */
 
 }
